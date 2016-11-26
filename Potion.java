bff0837203aef91b2bece15fbf5e772ec4d6279a
ArifRoktim <aroktim@stuy.edu>
2016-11-25 19:34:42 -0500

    Implemented classes Item and Potion.

---
+++ Potion.java
@@ -0,0 +1,8 @@
+//Team Grandmaster Fishers -- Youki Wu, Areef Roktim, Levee Olevsky
+//APCS1 pd4
+//HW35 -- Ye Olde Role Playing Game, Realized
+//2016-11-28
+
+public class Potion extends Item{
+    //will implement later
+}
